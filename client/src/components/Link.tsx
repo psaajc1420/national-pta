4a4b55ae1de24b132cac700ee9e393b617164279
Jorge Hinojosa <jorge94@live.com>
2022-01-31 18:00:17 -0600

    created Link

---
+++ client/src/components/Link.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+interface BaseLinkProps {
+	children: React.ReactNode;
+	to: string;
+}
+
+const BaseLink = ({ children, to }: BaseLinkProps) => {
+	return <StyledLink to={to}>{children}</StyledLink>;
+};
+
+export default BaseLink;
+
+const StyledLink = styled(Link)(() => ({
+	textDecoration: 'none',
+}));
