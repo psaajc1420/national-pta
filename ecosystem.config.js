07228f76fab88cbed70519eaabd5ab9bb88ca03c
Jacob Cadena <psaajc1420@yahoo.com>
2022-02-14 07:28:08 -0600

    style: fix spacing of braces

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,9 +12,9 @@ module.exports = {
       script: 'serve',
       env: {
       	PM2_SERVE_PATH: './build',
-	PM2_SERVE_PORT: 3000,
+	      PM2_SERVE_PORT: 3000,
       },
-     },
+    },
   ],
 };
 
