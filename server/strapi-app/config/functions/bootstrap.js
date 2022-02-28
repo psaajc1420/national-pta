b8228fba5085a77f7e6ca8562ad924299cd04208
Jacob Cadena <psaajc1420@yahoo.com>
2022-02-28 05:06:07 -0600

    fix merge conflict

--- server/strapi-app/config/functions/bootstrap.js
+++ server/strapi-app/config/functions/bootstrap.js
@@ -144,9 +144,12 @@ const createQuestions = async () => {
       if (answerData[key]) {
           answers = answerData[key];
       }
-
+      const  {text, question_type, order_id} = jsonData[key];
+      // console.log(order_id);
       await strapi.services.question.create({
-        text: jsonData[key],
+        text: text,
+        question_type: question_type,
+        order_id: order_id,
         answers: answers
       }).then((result) => {
         return result;
@@ -157,7 +160,6 @@ const createQuestions = async () => {
 }
 
 const createAnswers = async () => {
-
   const answers = await strapi.query("answer").find();
   
   if (answers.length === 0) {
@@ -177,6 +179,31 @@ const createAnswers = async () => {
   }
 }
 
+const createQuestionType = async () => {
+  // Check if age groups exist
+  const questionTypes = await strapi.query("question-type").find();
+
+  if (questionTypes.length === 0) {
+
+    let types = ["checkboxes", "yes-no", "fill-in-blank", "slider"];
+
+    for (let i = 0; i < types.length; i++) {
+      if (!questionTypes.includes(types[i])) {
+        await strapi.services["question-type"].create({
+          name: types[i],
+        });
+      }
+    }
+  }
+  // questionTypes.forEach((data) => {
+  //   if (types.includes(data.group)) {
+  //     types = types.filter((item) => item !== data.group);
+  //   }
+  // });
+
+
+};
+
 const createSlides = async () => {
   const slides = await strapi.query("slide").find();
 
@@ -187,12 +214,19 @@ const createSlides = async () => {
     for (const key in jsonData) {
       const questions = jsonData[key].questions;
 
+      
+      for (let j = 0; j < questions.length; j++) {
+        await strapi.services.question.update({id : questions[j]}, {
+          order_id: j+1
+        });
+      }
+
       if (questions) {
         await strapi.services.slide.create({
           slide_number: i+1,
           header: jsonData[key].header,
           age_group: 1,
-          questions: jsonData[key].questions
+          questions: questions
         }).then((result) => {
           return result;
         }).catch((error) => {strapi.log.info(`Error: ${error}`)});
@@ -210,6 +244,7 @@ module.exports = async () => {
     await createAdminUser();
   }
   await createAgeGroup();
+  await createQuestionType();
   await createQuestions();
   await createAnswers();
   await createSlides();
